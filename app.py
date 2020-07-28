f2448397045527ed90fd97f718ba9d2655a2be7d
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-07-28 17:41:17 +0300

    Initial deploy

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return render_template('syrinx/index.html')
+
+if __name__ == '__name__':
+  app.debug = True
+  app.run()
