e5848539a3f34a9b56d9eaadac85e6f7105e973f
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-07-30 17:22:23 +0300

    database alustus

--- app.py
+++ app.py
@@ -55,6 +55,35 @@ def logout():
   session.pop('useremail', None)
   return redirect('/')
 
+
+class Kayttaja(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  useremail = db.Column(db.String(50), nullable=False, unique=True)
+  password = db.Column(db.String(50), nullable=False)
+  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
+
+  suoritukset = db.relationship('Suoritus', backref='kayttaja')
+
+class Tehtava(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  kuvaus = db.Column(db.String(255), nullable=False)
+  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
+  num = db.Column(db.Integer)
+
+class Suoritus(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  id_user = db.Column(db.Integer, db.ForeignKey('kayttaja.id'), nullable=False)
+  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
+  checked = db.Column(db.Boolean, nullable=False)
+  add_date = db.Column(db.DateTime, nullable=False)
+  checked_date = db.Column(db.DateTime)
+
+class Jarjesto(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+
+  tehtavat = db.relationship('Tehtava', backref='jarjesto')
+
 if __name__ == '__name__':
   #app.debug = True
   app.run()
