18cbfb638c525acf9ef8a95ceb37fc89085a62ad
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-07-30 16:20:27 +0300

    logout

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,7 +7,7 @@ sala = 'test'
 userid = '123456'
 
 app = Flask(__name__)
-app.secret_key = b'@T$6bs3x2cm2F9X/rm47%8'
+app.secret_key = b.os.environ.get('SECRET_KEY', None)
 
 @app.route('/')
 def index():
