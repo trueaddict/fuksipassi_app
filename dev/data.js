491393f39d91c74175c51c30e55c22252b744636
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-02 18:21:10 +0300

    testi datan lisäys gittiin

---
+++ dev/data.js
@@ -0,0 +1,1 @@
+data = {"user" : "TEst Username", "tehtavat" : [{"nro":1, "kuvaus":"Liity Syrinx Ry:n jäseneksi", "suoritettu":"true", "id":1255353}, {"nro":2, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255354}, {"nro":3, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255355}]}
