2932b27e4cfa313f94732cd1af72d118d0745c21
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-10 21:08:39 +0300

    Hallintaan pyyntöjen ulkoasun hahmotelma

--- dev/datahallinta.js
+++ dev/datahallinta.js
@@ -2,19 +2,19 @@ data = {
   "kuitattavat": [
     {
       "id_teht": 1255355, 
-      "id_user": 123456,
+      "id_user": 1,
       "message":"kävin syömässä maijassa"
     },
     {
       "id_teht": 1255354, 
-      "id_user": 123456,
+      "id_user": 1,
       "message":"selitys"
     }
   ],
   "tehtavat" : [{"nro":1, "kuvaus":"Liity Syrinx Ry:n jäseneksi", "suoritettu":"true", "id":1255353}, {"nro":2, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255354}, {"nro":3, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255355}],
   "kayttajat" : [
     {
-      "id" : 123456,
+      "id" : 1,
       "useremail" : "test",
       "id_jarj": 99999 
     }
--- static/script/hallinta.js
+++ static/script/hallinta.js
@@ -3,4 +3,30 @@ console.log(data);
 window.onload = function() {
   M.AutoInit();
   //var instance = M.Tabs.init(el, options);
+  luoPyynnot();
+}
+
+
+function luoPyynnot() {
+  for (let i = 0; i < 10; i++) {
+    document.getElementById('collapsible').appendChild(luoPyynto(i+1));
+  }
+}
+
+
+function luoPyynto(i) {
+  var li1=document.createElement('li');
+  var div1=document.createElement('div');
+  div1.className='collapsible-header';
+  li1.appendChild(div1);
+  var txt2=document.createTextNode('Nimi Suku '+ i);
+  div1.appendChild(txt2);
+  var div2=document.createElement('div');
+  div2.className='collapsible-body';
+  li1.appendChild(div2);
+  var p1=document.createElement('p');
+  div2.appendChild(p1);
+  var txt5=document.createTextNode('Lorem ipsum');
+  p1.appendChild(txt5);
+  return li1;
 }
