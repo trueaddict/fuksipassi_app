d89d5db5b891ba03d25e5fba54951d907e2f7d05
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-12 22:30:20 +0300

    tehtavan kuitattavaksi lähettäminen

--- app.py
+++ app.py
@@ -133,9 +133,8 @@ def tarkista():
     id_jarj = session['id_jarj']
     message = request.form['message']
     pyynto = Suoritus(id=int(str(id_teht)+str(id_user)+str(id_jarj)), id_teht=id_teht, id_user=id_user, id_jarj=id_jarj, info_text=message)
-    print(pyynto)
-    #db.session.add(pyynto)
-    #db.session.commit()
+    db.session.add(pyynto)
+    db.session.commit()
     return redirect('/etusivu?id='+request.form['id']+'#'+request.form['id'])
   else:
     return render_template('index.html', data='Kirjaudu sisään!')
