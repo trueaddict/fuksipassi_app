825a5a3d5f71132c184c053718544b3d5431b327
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-13 17:35:53 +0300

    hallinta datan luonti

--- app.py
+++ app.py
@@ -168,23 +168,9 @@ def logouthallinta():
 # HALLINTA END
 
 def generateDataHallinta():
+  suoritukset = Suoritus.query.all()
   return {
-          "kuitattavat": [
-            {
-              "id_teht": 1255355, 
-              "id_user": 1,
-              "useremail": "test",
-              "kuvaus": "Käy syömässä Maijassa",
-              "message":"kävin syömässä maijassa"
-            },
-            {
-              "id_teht": 1255354, 
-              "id_user": 1,
-              "useremail": "test",
-              "kuvaus": "Liity Syrinx Ry:n jäseneksi",
-              "message":"selitys"
-            }
-          ],
+          "kuitattavat": suoritukset,
           "tehtavat" : [{"nro":1, "kuvaus":"Liity Syrinx Ry:n jäseneksi", "suoritettu":"true", "id":1255353}, {"nro":2, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255354}, {"nro":3, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255355}],
           "kayttajat" : [
             {
