92fbea4dfaefee3f0884f5f39506948d89cc0e1f
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-18 20:11:17 +0300

    demo data

---
+++ dev/datahallinta.js
@@ -0,0 +1,26 @@
+data = {
+  "kuitattavat": [
+    {
+      "id_teht": 1255355, 
+      "id_user": 1,
+      "useremail": "test",
+      "kuvaus": "Käy syömässä Maijassa",
+      "message":"kävin syömässä maijassa"
+    },
+    {
+      "id_teht": 1255354, 
+      "id_user": 1,
+      "useremail": "test",
+      "kuvaus": "Liity Syrinx Ry:n jäseneksi",
+      "message":"selitys"
+    }
+  ],
+  "tehtavat" : [{"nro":1, "kuvaus":"Liity Syrinx Ry:n jäseneksi", "suoritettu":"true", "id":1255353}, {"nro":2, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255354}, {"nro":3, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255355}],
+  "kayttajat" : [
+    {
+      "id" : 1,
+      "useremail" : "test",
+      "id_jarj": 99999 
+    }
+  ]
+}
