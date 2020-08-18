08885958ed765ffcdcd68e478169a036e21e29f1
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-18 19:42:24 +0300

    init

--- static/script/hallinta.js
+++ static/script/hallinta.js
@@ -47,7 +47,7 @@ function luoPyynto(i) {
   li1.appendChild(div2);
   
   var form = document.createElement('form');
-  form.setAttribute('action', '/hallinta/kuittaa');
+  form.setAttribute('action', '/kuittaa');
   form.setAttribute('method', 'post');
   div2.appendChild(form);
 
