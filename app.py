675c20ca7972a35129ea81f0749c628044587f89
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-20 20:04:07 +0300

    yht hallinta

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def logouthallinta():
 # HALLINTA END
 
 def generateDataHallinta():
-  suoritukset = Suoritus.query.filter(Suoritus.id_jarj==idjarj).all()
+  suoritukset = Suoritus.query.all()
   teht = Tehtava.query.filter(Tehtava.id_jarj==idjarj).all()
   kayttajat = Kayttaja.query.filter(Kayttaja.id_jarj==idjarj).all()
   kayt_list = dict()
@@ -155,8 +155,9 @@ def generateDataHallinta():
     kayt_list[k.id] = k.useremail
 
   for s in suoritukset:
-    if not s.checked:
-      suor_list.append({"id_user":s.id_user, "id_teht":s.id_teht, "useremail":kayt_list.get(s.id_user), "kuvaus":teht_list.get(s.id_teht).rstrip(), "message":s.info_text})
+    if s.id_jarj == idjarj:
+      if not s.checked:
+        suor_list.append({"id_user":s.id_user, "id_teht":s.id_teht, "useremail":kayt_list.get(s.id_user), "kuvaus":teht_list.get(s.id_teht).rstrip(), "message":s.info_text})
   return {
           "kuitattavat": suor_list,
           "tehtavat" : [{"nro":1, "kuvaus":"Liity Syrinx Ry:n jäseneksi", "suoritettu":"true", "id":1255353}, {"nro":2, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255354}, {"nro":3, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255355}],
