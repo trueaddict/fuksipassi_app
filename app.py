3e366182b869b17e6480830e004acb7a4692e447
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-20 20:43:03 +0300

    yht hallinta

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ class Jarjesto(db.Model):
 def hallinta():
   if request.method == 'GET':
     if 'loggedin' in session and 'hallinta' in session:
-      data = generateDataHallinta(idjarj)
+      data = generateDataHallinta()
       return render_template('/' + ainejarjesto + '/index.html', data=json.dumps(data))
     return render_template('/index.html', data='')
   if request.method == "POST" and 'useremail' in request.form and 'password' in request.form:
@@ -106,7 +106,7 @@ def hallinta():
       session['hallinta'] = True
       session['id'] = idjarj
       session['useremail'] = username
-      data = generateDataHallinta(idjarj)
+      data = generateDataHallinta()
       return render_template('/' + ainejarjesto + '/index.html', data=json.dumps(data))
     return render_template('/index.html', data='Väärä sähköposti tai salasana!')
 
@@ -124,10 +124,10 @@ def kuittaa():
             suor.checked = True
             suor.checked_date = datetime.today()
             db.session.commit()
-    data = generateDataHallinta(idjarj)
+    data = generateDataHallinta()
     return render_template('/' + ainejarjesto + '/index.html', data=json.dumps(data))
   if request.method == 'GET' and 'loggedin' in session and 'hallinta' in session:
-    data = generateDataHallinta(idjarj)
+    data = generateDataHallinta()
     return render_template('/' + ainejarjesto + '/index.html', data=json.dumps(data))
   return render_template('/index.html', data='Kirjaudu sisään!')
 
@@ -140,7 +140,8 @@ def logouthallinta():
 
 # HALLINTA END
 
-def generateDataHallinta(jarj):
+def generateDataHallinta():
+  jarj = session['id']
   suoritukset = Suoritus.query.all()
   teht = Tehtava.query.all()
   kayttajat = Kayttaja.query.all()
