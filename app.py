03a0565a7dada9529719b5d4b453a0c2462542f9
trueaddict <50746660+trueaddict@users.noreply.github.com>
2020-08-20 19:34:59 +0300

    yht hallinta

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def generateDataHallinta():
 
   for s in suoritukset:
     if not s.checked:
-      suor_list.append({"id_user":s.id_user, "id_teht":s.id_teht, "useremail":kayt_list.get(s.id_user), "kuvaus":teht_list.get(s.id_teht), "message":s.info_text})
+      suor_list.append({"id_user":s.id_user, "id_teht":s.id_teht, "useremail":kayt_list.get(s.id_user), "kuvaus":teht_list.get(s.id_teht).rstrip(), "message":s.info_text})
   return {
           "kuitattavat": suor_list,
           "tehtavat" : [{"nro":1, "kuvaus":"Liity Syrinx Ry:n jäseneksi", "suoritettu":"true", "id":1255353}, {"nro":2, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255354}, {"nro":3, "kuvaus":"Osallistu tapahtumaan", "suoritettu":"false", "id":1255355}],
