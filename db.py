89db86413f57b5b7e46dcf585f12a3f7d4c29424
trueaddict <ville.otto@gmail.com>
2021-07-04 15:16:25 +0300

    db uri

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 app.secret_key = os.environ.get('SECRET_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 
 db = SQLAlchemy(app)
 
