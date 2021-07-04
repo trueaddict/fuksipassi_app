e8fbc38d439a96b63de2a10001436bb62a2961f8
trueaddict <ville.otto@gmail.com>
2021-07-04 14:47:14 +0300

    new col

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, send_from_directory, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS, cross_origin
-import datetime
 import os
+from db import create_app, create_new_column, Kayttaja, Tehtava, Suoritus, Jarjesto
+
 
 app = Flask(__name__, static_folder='client/build', static_url_path='')
 app.secret_key = os.environ.get('SECRET_KEY')
@@ -10,69 +10,8 @@ app.secret_key = os.environ.get('SECRET_KEY')
 cors = CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') 
-db = SQLAlchemy(app)
+app = create_app(app)
 
-class Kayttaja(db.Model):
-  __tablename__ = 'kayttaja'
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  useremail = db.Column(db.String(50), nullable=False, unique=True)
-  password = db.Column(db.String(50), nullable=False)
-  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
-
-  suoritukset = db.relationship('Suoritus', backref='kayttaja')
-
-  def __init__(self, useremail, password, id_jarj):
-    self.id = None
-    self.useremail = useremail
-    self.password = password
-    self.id_jarj = id_jarj
-
-class Tehtava(db.Model):
-  __tablename__ = 'tehtava'
-  id = db.Column(db.Integer, primary_key=True)
-  kuvaus = db.Column(db.String(255), nullable=False)
-  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
-  num = db.Column(db.Integer)
-  tyyppi = db.Column(db.String(100))
-
-  def __init__(self, id, kuvaus, id_jarj, tyyppi, num):
-    self.id = id
-    self.kuvaus = kuvaus
-    self.id_jarj = id_jarj
-    self.num = num
-    self.tyyppi = tyyppi
-
-class Suoritus(db.Model):
-  __tablename__ = 'suoritus'
-  id = db.Column(db.Integer, primary_key=True)
-  id_user = db.Column(db.Integer, db.ForeignKey('kayttaja.id'), nullable=False)
-  id_teht = db.Column(db.Integer)
-  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
-  add_date = db.Column(db.DateTime, nullable=False)
-  info_text = db.Column(db.String(100))
-  checked = db.Column(db.Boolean, nullable=False)
-  checked_date = db.Column(db.DateTime)
-
-  def __init__(self, id, id_user, id_teht, id_jarj, info_text):
-    self.id = id
-    self.id_user = id_user
-    self.id_teht = id_teht
-    self.id_jarj = id_jarj
-    self.checked = False
-    self.add_date = datetime.today()
-    self.checked_date = None
-    self.info_text = info_text
-
-class Jarjesto(db.Model):
-  __tablename__ = 'jarjesto'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(50), nullable=False)
-  password = db.Column(db.String(50), nullable=False)
-  tehtavat = db.relationship('Tehtava', backref='jarjesto')
-
-  def __init__(self, id, name):
-    self.id = id
-    self.name = name
 
 
 @app.route('/')
@@ -132,6 +71,7 @@ def createNewUser(useremail, jarj):
   db.session.add(newUser)
   db.session.commit()
   return newUser
+
 
 if __name__ == '__name__':
   app.debug = True
---
+++ db.py
@@ -0,0 +1,77 @@
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+db = SQLAlchemy()
+
+def create_app(app):
+    column = db.Column(db.String(50), nullable=False)
+    create_new_column(db, 'jarjesto', column)
+    db.init_app(app)
+    return app
+
+def create_new_column(engine, table_name, column):
+    column_name = column.compile(dialect=engine.dialect)
+    column_type = column.type.compile(engine.dialect)
+    engine.execute('ALTER TABLE %s ADD COLUMN %s %s' % (table_name, column_name, column_type))
+
+class Kayttaja(db.Model):
+  __tablename__ = 'kayttaja'
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  useremail = db.Column(db.String(50), nullable=False, unique=True)
+  password = db.Column(db.String(50), nullable=False)
+  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
+
+  suoritukset = db.relationship('Suoritus', backref='kayttaja')
+
+  def __init__(self, useremail, password, id_jarj):
+    self.id = None
+    self.useremail = useremail
+    self.password = password
+    self.id_jarj = id_jarj
+
+class Tehtava(db.Model):
+  __tablename__ = 'tehtava'
+  id = db.Column(db.Integer, primary_key=True)
+  kuvaus = db.Column(db.String(255), nullable=False)
+  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
+  num = db.Column(db.Integer)
+  tyyppi = db.Column(db.String(100))
+
+  def __init__(self, id, kuvaus, id_jarj, tyyppi, num):
+    self.id = id
+    self.kuvaus = kuvaus
+    self.id_jarj = id_jarj
+    self.num = num
+    self.tyyppi = tyyppi
+
+class Suoritus(db.Model):
+  __tablename__ = 'suoritus'
+  id = db.Column(db.Integer, primary_key=True)
+  id_user = db.Column(db.Integer, db.ForeignKey('kayttaja.id'), nullable=False)
+  id_teht = db.Column(db.Integer)
+  id_jarj = db.Column(db.Integer, db.ForeignKey('jarjesto.id'), nullable=False)
+  add_date = db.Column(db.DateTime, nullable=False)
+  info_text = db.Column(db.String(100))
+  checked = db.Column(db.Boolean, nullable=False)
+  checked_date = db.Column(db.DateTime)
+
+  def __init__(self, id, id_user, id_teht, id_jarj, info_text):
+    self.id = id
+    self.id_user = id_user
+    self.id_teht = id_teht
+    self.id_jarj = id_jarj
+    self.checked = False
+    self.add_date = datetime.today()
+    self.checked_date = None
+    self.info_text = info_text
+
+class Jarjesto(db.Model):
+  __tablename__ = 'jarjesto'
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  password = db.Column(db.String(50), nullable=False)
+  tehtavat = db.relationship('Tehtava', backref='jarjesto')
+
+  def __init__(self, id, name):
+    self.id = id
+    self.name = name
