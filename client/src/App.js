d101220bccd39e4d0a23c0f143402364973f9321
trueaddict <ville.otto@gmail.com>
2021-07-04 14:31:48 +0300

    url

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
             <Login setToken={setToken} id_jarj={'1971'}/>
           </Route>
           <Route path='/'>
-            <Login setToken={setToken} id_jarj={'1971'}/>
+            <p>Public frontpage</p>
           </Route>
         </Switch>
       
